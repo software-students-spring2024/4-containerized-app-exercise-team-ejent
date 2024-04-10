6e9b3a921d0943f52c3f0a61251ba0280ba77c0f
Jean Luis Adrover Torres <98844818+jladrover@users.noreply.github.com>
2024-04-10 00:55:15 -0400

    edits made

--- machine-learning-client/client.py
+++ machine-learning-client/client.py
@@ -1,10 +1,25 @@
 import os
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 from fer import FER
 import cv2
-os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 
-img = cv2.imread("./testml.png")
-detector = FER(mtcnn=True)
-print(detector.detect_emotions(img))
-#emotion, score = detector.top_emotion(img)
-#print(emotion, score)
+
+def get_emotion(image):
+    detector = FER(mtcnn=True)
+    emotions = detector.detect_emotions(image)
+    i = 1
+    res = ""
+    for box in emotions:
+        max_emotion = max(box['emotions'], key=lambda x: box['emotions'][x])
+        res += (f"Person {i} seems to be feeling {max_emotion}\n")
+        i += 1
+    return res
+
+#im = cv2.imread('./test1.png')
+#print(get_emotion(im))
+"""
+machine-learning-client % python client.py
+Person 1 is feeling happy
+Person 2 is feeling happy
+Person 3 is feeling sad
+"""
