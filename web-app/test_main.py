4ea45afe7785acb55a537b4e939572262e2543a8
sik247 <sik247@nyu.edu>
2024-04-16 03:17:46 -0400

    pylint

--- web-app/test_main.py
+++ web-app/test_main.py
@@ -1,24 +1,25 @@
-import base64
+# pylint: disable=all
+"""
+Machine learning client for detecting emotions in images.
+This client connects to a MongoDB Atlas database, retrieves images.
+"""
 import io
 import pytest
-from flask import Flask
 from main import app
-from werkzeug.datastructures import FileStorage
 
-# pylint: disable=all
 @pytest.fixture
 def client():
+    """Client"""
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
-
+# pylint: disable=redefined-outer-name
 def test_index(client):
     """Test the index route"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"<!DOCTYPE html>\n<html>\n<head>\n" in response.data
-
 
 def test_upload_file_no_photo(client):
     """Test the /upload endpoint with no photo"""
@@ -26,24 +27,9 @@ def test_upload_file_no_photo(client):
     assert response.status_code == 200
     assert b"No photo provided" in response.data
 
-
 def test_upload_file_no_name(client):
     """Test the /upload endpoint with no name"""
     data = {"photo": (io.BytesIO(b"this is a test"), "test.jpg")}
     response = client.post("/upload", data=data)
     assert response.status_code == 200
     assert b"No name provided" in response.data
-
-
-# def test_upload_file_with_photo(client):
-#     """Test the /upload endpoint with a photo"""
-#     data = {"name": "test", "photo": (io.BytesIO(b"this is a test image"), "test.jpg")}
-#     response = client.post("/upload", data=data, content_type="multipart/form-data")
-#     assert response.status_code == 200
-#     assert b"<!DOCTYPE html>\n<html>\n<head>\n" in response.data
-
-
-# def test_result(client):
-#     """Test the /result endpoint with no processed document"""
-#     response = client.get("/result")
-#     assert response.status_code == 200
