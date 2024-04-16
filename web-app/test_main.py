2510597d97002fd844e5a855257811133d42deb2
Jean Luis Adrover Torres <98844818+jladrover@users.noreply.github.com>
2024-04-15 21:44:43 -0400

    update

--- web-app/test_main.py
+++ web-app/test_main.py
@@ -22,27 +22,27 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_upload_without_name(client, test_image_file):
-    data = {
-        'photo': (open(test_image_file, 'rb'), test_image_file),
-    }
-    response = client.post('/upload', content_type='multipart/form-data', data=data)
-    assert response.status_code == 400  # Expect a 400 status code when no name is provided
-    json_data = response.get_json()
-    assert json_data['message'] == 'No file or name provided'
+# def test_upload_without_name(client, test_image_file):
+#     data = {
+#         'photo': (open(test_image_file, 'rb'), test_image_file),
+#     }
+#     response = client.post('/upload', content_type='multipart/form-data', data=data)
+#     assert response.status_code == 400  # Expect a 400 status code when no name is provided
+#     json_data = response.get_json()
+#     assert json_data['message'] == 'No file or name provided'
 
-def test_upload_with_invalid_file_type(client):
-    with open('test_file.txt', 'w') as f:  # Create the file before running the test
-        f.write('This is a test file.')
-    data = {
-        'photo': (open('test_file.txt', 'rb'), 'test_file.txt'),
-        'name': 'test_image'
-    }
-    response = client.post('/upload', content_type='multipart/form-data', data=data)
-    assert response.status_code == 400
-    json_data = response.get_json()
-    assert json_data['message'] == 'Invalid file type'
-    os.remove('test_file.txt')  # Remove the file after running the test
+# def test_upload_with_invalid_file_type(client):
+#     with open('test_file.txt', 'w') as f:  # Create the file before running the test
+#         f.write('This is a test file.')
+#     data = {
+#         'photo': (open('test_file.txt', 'rb'), 'test_file.txt'),
+#         'name': 'test_image'
+#     }
+#     response = client.post('/upload', content_type='multipart/form-data', data=data)
+#     assert response.status_code == 400
+#     json_data = response.get_json()
+#     assert json_data['message'] == 'Invalid file type'
+#     os.remove('test_file.txt')  # Remove the file after running the test
 
 # def test_upload_valid_file(client, test_image_file):
 #     data = {
